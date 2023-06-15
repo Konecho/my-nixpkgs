399dffde84f0d780c007c80e3b26ac0b2bc98a74
NixOS <me@meiro.top>
2023-06-15 16:11:21 +0800

    change to package

--- pkgs/pokebase/default.nix
+++ pkgs/pokebase/default.nix
@@ -3,7 +3,7 @@
   python3,
   fetchFromGitHub,
 }:
-python3.pkgs.buildPythonApplication rec {
+python3.pkgs.buildPythonPackage rec {
   pname = "pokebase";
   version = "1.3.0";
   format = "setuptools";
