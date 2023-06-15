963304a00d3425d37f5192eef2493997c38dbf7f
NixOS <me@meiro.top>
2023-06-15 21:44:26 +0800

    fetch github

--- pkgs/bgmi/anime-episode-parser.nix
+++ pkgs/bgmi/anime-episode-parser.nix
@@ -1,16 +1,18 @@
 {
   lib,
   python3,
-  fetchPypi,
+  fetchFromGitHub,
 }:
 python3.pkgs.buildPythonPackage rec {
   pname = "anime-episode-parser";
   version = "0.0.12";
   format = "pyproject";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-JcK45AP2DbRuIwCY9cuRlzi6g9MbjwTJWGSk+ZaD1/M=";
+  src = fetchFromGitHub {
+    owner = "BGmi";
+    repo = "anime-episode-parser";
+    rev = "v${version}";
+    hash = "sha256-zfhEW9QzGtx7d0Oxx2d5oAG2NFIznpEQ2MkgqyFnLnM=";
   };
 
   nativeBuildInputs = [
